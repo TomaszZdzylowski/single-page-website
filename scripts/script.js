034a1b4b1a3aeb6da0bd35a255a834f899a98a9c
Tomasz Zdżyłowski <tomektomek53@gmail.com>
2020-04-21 18:51:13 +0200

    init new project

---
+++ scripts/script.js
@@ -0,0 +1,23 @@
+
+(() => {
+    const menuList = document.querySelector('.js-list');
+    let isClicked = false;
+
+    document.addEventListener('click', function (event) {
+        if (event.target.closest('.js-bar')) {
+            menuList.classList.toggle('h-hide');
+            return isClicked = !isClicked;
+        }
+        else if (isClicked == true) {
+            if ((event.target.closest('body') && !event.target.closest('.js-list'))) {
+                menuList.classList.toggle('h-hide');
+                return isClicked = !isClicked;
+
+            }
+        }
+
+
+    });
+
+
+})()
